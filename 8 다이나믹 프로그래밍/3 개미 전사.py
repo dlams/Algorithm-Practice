54d4325c110131862cdc922df23aa7023bfe965d
dlams <dlalsdn1009@naver.com>
2022-12-07 09:04:43 +0900

    Add lore of 8

--- 8 다이나믹 프로그래밍/3 개미 전사.py
+++ 8 다이나믹 프로그래밍/3 개미 전사.py
@@ -6,6 +6,8 @@ dp = [0] * 100
 dp[0] = array[0]
 dp[1] = max(array[0], array[1])
 for i in range(2, n):
+    # ㅁㅁㅁㅁㅁㅁㅁㅁ <-i-1 ㅁ
+    # i - 1번째 까지의 최대값과 i - 2까지의 최대값 + i를 비교 
     dp[i] = max(dp[i - 1], dp[i - 2] + array[i])
 
 print(dp[n - 1])
